0c23da9644fa957c3dd979314d76729dd536e0bc
arjnramesh281 <arjunramesh281@gmail.com>
2024-08-07 11:35:30 +0530

    html file handling

--- html/sample.py
+++ html/sample.py
@@ -1,5 +1,24 @@
-a=int(input("enter a number :"))
-if a//2*2==a:
-    print("even")
-else:
-    print("odd")
+# a=int(input("enter a number :"))
+# if a//2*2==a:
+#     print("even")
+# else:
+#     print("odd")
+
+
+
+# question number one
+
+
+for i in range(5):
+    spaces = " " * (5 - i - 1)
+    stars = "*" * (2 * i + 1)
+    print(spaces + stars)
+
+
+
+# question number two
+
+# l=[5,4,6,3,2,5,1]
+# for i in l:
+#     print(i* "*",end=" ")
+#     print()
